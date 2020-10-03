c4b55692cc752bb19b9e0fb5a15eac74d76e423f
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-02 23:19:51 -0400

    add card component for monster info

--- react-course/monsters-rolodex/src/App.js
+++ react-course/monsters-rolodex/src/App.js
@@ -25,9 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='kevin'>
-        {this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
