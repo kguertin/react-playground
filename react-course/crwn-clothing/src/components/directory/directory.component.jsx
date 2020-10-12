729bef69221038b3282288ec3970495a875c2094
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-12 17:17:19 -0400

    enable link to hats page and use withRouter to provided react router functionality

--- react-course/crwn-clothing/src/components/directory/directory.component.jsx
+++ react-course/crwn-clothing/src/components/directory/directory.component.jsx
@@ -14,7 +14,7 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -44,13 +44,13 @@ class Directory extends React.Component {
                 }
             ]
         }
-    }
+    }yarn 
 
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({title, imageUrl, id, size}) => {
-                   return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...sectionsProps}) => {
+                   return <MenuItem key={id} {...sectionsProps} />
                 })}
             </div>
         )
