7aacd88866fe2457b4c0bfc5dc89a0089de581f3
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-13 13:31:21 -0400

    make form input component

---
+++ react-course/crwn-clothing/src/components/form-input/form-input.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...inputProps}) => (
+    <div className="group">
+        <input className="form-input" onChange={handleChange} type="text" {...inputProps}/>
+        {
+            label ? 
+            (<label className={`${inputProps.value.length ? 'shrink' : ''} form-input-label`} >
+            {label}
+            </label>)
+            : null
+        }
+    </div>
+)
+
+export default FormInput;
