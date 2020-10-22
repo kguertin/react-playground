64eb553d9f4854e84e92ac9a56d87ebf599981d2
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-22 17:28:27 -0400

    create checkout page and build cart total selector

--- react-course/crwn-clothing/src/redux/cart/cart.selectors.js
+++ react-course/crwn-clothing/src/redux/cart/cart.selectors.js
@@ -10,3 +10,8 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((accumulatedTotal, cartItem) => accumulatedTotal + cartItem.price * cartItem.quantity, 0)
+)
