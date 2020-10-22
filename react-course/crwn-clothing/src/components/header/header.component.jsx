44c9d9b6fe83135035f0e72043e5a02b8e02866d
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-22 11:02:57 -0400

    create selector toget total quantity for items in cart

--- react-course/crwn-clothing/src/components/header/header.component.jsx
+++ react-course/crwn-clothing/src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.componennt';
+import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
