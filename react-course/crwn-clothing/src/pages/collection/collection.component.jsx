77ddc7265a23b9dc60d5824dc282f77e4d757f4d
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-26 13:16:37 -0400

    create id map and select collection using shop selector to get correct item colleection

--- react-course/crwn-clothing/src/pages/collection/collection.component.jsx
+++ react-course/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
+    return (
     <div className="category">
-        <h2>{match.params.collectionId}</h2>
+        <h2>Collection Page</h2>
     </div>
-)
+    )
+}
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
