018bbf0e104d78f57fe1c9db319259c2e3bb87ce
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-26 19:13:16 -0400

    change selector that allows for the transfer of data to the preview component

--- react-course/crwn-clothing/src/redux/shop/shop.selectors.js
+++ react-course/crwn-clothing/src/redux/shop/shop.selectors.js
@@ -5,6 +5,10 @@ const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
 
+export const selectCollectionsForPreview = createSelector([selectCollections], collections => {
+    return Object.keys(collections).map(key => collections[key])
+});
+
 export const selectCollection = memoize(collectionUrlParam => {
     return createSelector([selectCollections], collections => collections[collectionUrlParam])
 });
