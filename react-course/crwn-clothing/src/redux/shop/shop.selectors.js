2e293b79a73798392e7fad89d23b642d190f7724
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-26 16:34:10 -0400

    normalize our directory data form more performant data recall

--- react-course/crwn-clothing/src/redux/shop/shop.selectors.js
+++ react-course/crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,19 +1,10 @@
+import memoize from 'lodash.memoize'
 import { createSelector } from 'reselect';
-
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
 
-export const selectCollection = collectionUrlParam => {
-    return createSelector([selectCollections], collections => {
-        return collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]);
-    }
-)};
+export const selectCollection = memoize(collectionUrlParam => {
+    return createSelector([selectCollections], collections => collections[collectionUrlParam])
+});
