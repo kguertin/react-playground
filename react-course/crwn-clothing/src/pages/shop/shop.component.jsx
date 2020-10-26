d745f2d65b521c2258f2e140bd84b03a73beccdc
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-25 21:04:56 -0400

    refactor collections preview to be component in shop page

--- react-course/crwn-clothing/src/pages/shop/shop.component.jsx
+++ react-course/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors'
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
     <div className="shop-page">
-        {collections.map(({id, ...collectionProps}) => (
-            <CollectionPreview key={id} {...collectionProps} />
-        ))}
+        <CollectionsOverview />
     </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
