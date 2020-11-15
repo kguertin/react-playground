a760d7e46b80db47a9c94280a2a8bb2c5446972c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-15 16:51:43 -0500

    move async call for collection data into redux and created reducers for various async loading types

--- react-course/crwn-clothing/src/redux/store.js
+++ react-course/crwn-clothing/src/redux/store.js
@@ -1,16 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middleWares = [logger];
+const middleWares = [logger, thunk];
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export const persistor = persistStore(store);
 
 export default {
-    store,
-    persistor
-}
+  store,
+  persistor,
+};
