31a9841b8657c3376fccabc2b28ce42afa1ad9b8
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-11-27 18:20:43 -0500

    create pages

---
+++ gatsby-tutorial/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Here is something about me.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ gatsby-tutorial/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Here is my contact information</p>
+    </div>
+  )
+}
+
+export default ContactPage
