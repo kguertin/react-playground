9d4a7fe4eb745ad90c4abec85fca3b80c029e06a
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-11 17:27:22 -0400

    use navigation

--- react-native-basics/rn-starter/src/screens/HomeScreen.js
+++ react-native-basics/rn-starter/src/screens/HomeScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Scottie Barnes ROY</Text>
       <Button
         title="Go to Components Demo"
-        onPress={() => console.log("Button Pressed")}
+        onPress={() => navigation.navigate("Components")}
       />
-      <TouchableOpacity onPress={() => console.log("List Pressed")}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
     </View>
   );
 };
