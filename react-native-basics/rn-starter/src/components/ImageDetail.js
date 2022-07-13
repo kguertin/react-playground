ed9d6f95665e1d7a153d6b96b75416c6b4f54970
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-13 14:23:01 -0400

    add images

--- react-native-basics/rn-starter/src/components/ImageDetail.js
+++ react-native-basics/rn-starter/src/components/ImageDetail.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const ImageDetail = ({ title }) => {
-  return <Text>{title}</Text>;
+const ImageDetail = ({ title, imageSource, score }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>{`Image Score - ${score}`}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
