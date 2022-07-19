49a04520c88b871654175b5b02ec4294ee851863
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-19 11:20:55 -0400

    state and text

---
+++ react-native-basics/rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const TextScreen = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(value) => setName(value)}
+      />
+      <Text>{`My name is ${name}`}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
+
+export default TextScreen;
