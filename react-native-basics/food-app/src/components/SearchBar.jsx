52a3c2d58d6d9596e3975d5a1d6b0c814a69c015
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-09-28 14:32:53 -0400

    style and set up state for searchbar

--- react-native-basics/food-app/src/components/SearchBar.jsx
+++ react-native-basics/food-app/src/components/SearchBar.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        placeholder="search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  inputStyle: { flex: 1, fontSize: 18 },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
 
 export default SearchBar;
--- react-native-basics/food-app/src/screens/SearchScreen.jsx
+++ react-native-basics/food-app/src/screens/SearchScreen.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term was submitted")}
+      />
+      <Text>{term}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+  background: { backgroundColor: "#FFF", flex: 1 },
+});
 
 export default SearchScreen;
